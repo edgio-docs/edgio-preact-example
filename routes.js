0c6d862ed18b08057af88c2fcf347fa331b2a677
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-08-13 13:17:33 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -5,6 +5,9 @@ const router = new Router()
 
 if (isProductionBuild()) {
   router.static('build')
+  router.fallback(({ serveStatic }) => {
+    serveStatic('build/index.html')
+  })
 } else {
   router.fallback(({ renderWithApp }) => {
     renderWithApp()
